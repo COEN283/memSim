67984d6ece331920b9bb92153e0275825ffd8711
rdness <dawei@ucdavis.edu>
2016-02-15 18:33:17 -0800

    Started Altron object

---
+++ altron.h
@@ -0,0 +1,80 @@
+/****************************************************************************************
+
+	Class Name:		
+		
+
+	Purpose:		
+
+	Author:			David Wei
+	Date:			
+	Last Modified:	Initial Revision
+	IDE USED:		gcc version 4.8.3 (cygwin64 on Windows 8.1)
+
+****************************************************************************************/
+
+#ifndef ALTRON_H
+#define ALTRON_H
+
+//***************************************************************************************
+// Class Definition
+//***************************************************************************************
+
+class Altron
+{
+	private:
+		unsigned int time;
+		// LL for elements
+		// Queue for incoming elements		
+
+	public:
+
+	// Altron Constructor
+	
+	// Read into Queue member function
+
+	// Increment time member function
+
+	// Count statistics
+};
+
+/*
+Altron
+{
+	// Create queue of allocation objects
+
+	// Create LL of allocation objects
+
+	// Setup output writer for Stats
+
+	// 
+}
+
+*/
+
+/* populateQueue
+{
+	// Open file pointer
+	ifstream infile(FILENAME.c_str()); 
+	
+	// check for successful open
+	if(!infile)
+	{
+		cerr << "Error in opening input file: " << FILENAME << endl;
+		exit(1);
+	}
+	
+	// read from data file
+	// insert into queue
+
+	// close infile
+	infile.close();
+}
+*/
+
+/* increment time
+{
+	// Read 
+
+}
+*/
+#endif
