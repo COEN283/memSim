cc773b82e387eafaeb7d7d76debaa935aa48b98b
ypcrane <sisiyang.inbox@gmail.com>
2016-02-28 17:07:00 -0800

    created firstfit allocation method

---
+++ firstFit.cpp
@@ -0,0 +1,68 @@
+/*
+ * firstFit.cpp
+ *
+ *  Created on: Feb 28, 2016
+ *      Author: Sisi Yang
+ *    IDE Used: Eclipse Luna 4.4.0 for C++ (Mac OSX El Capitan version 10.11.2)
+ *    
+ */
+//This program is created for COEN283 project.
+
+#include <firstFit.h>
+
+//constructor
+FirstFit::FirstFit(unsigned int start, unsigned int size, unsigned int finish)
+: Allocation(start, size, finish) {}
+
+//destructor
+FirstFit::~FirstFit() {}
+
+//member functions
+bool FirstFit::allocate()
+{
+	//keep track of size of empty memory
+	int count = 0;
+
+	for(int i = 0; i < MEM_SIZE; i++)
+	{
+		//if memory slot is empty, start counting
+		if(memArr[i] == 0)
+			count++;
+		//if memory slot is occupied, reset counter
+		else
+			count = 0;
+
+		//if counter is enough for the allocation object
+		if(count >= allocationSize)
+		{
+			//update memory location
+			memLoc = i - count;
+
+			//update memArr, mark occupied indexes as 1
+			for(int j = memLoc; j <= allocationSize; j++)
+				memArr[j] = 1;
+
+			return true;
+		}
+
+	}//ends for loop
+
+	//if no memory chunk is big enough to fit the allocation object
+	return false;
+
+
+}//ends allocate()
+
+bool FirstFit::deallocate()
+{
+	//starting at the memory location of the allocation object
+	for(int i = memLoc; i <= allocationSize; i++)
+	{
+		//indicate empty memory by setting memory slots to 0
+		memArr[i] = 0;
+
+	}//ends for loop
+
+	return true;
+
+}//ends deallocate()
---
+++ firstFit.h
@@ -0,0 +1,41 @@
+/*
+ * FirstFit.h
+ *
+ *  Created on: Feb 28, 2016
+ *      Author: Sisi Yang
+ *    IDE Used: Eclipse Luna 4.4.0 for C++ (Mac OSX El Capitan version 10.11.2)
+ *    
+ */
+//This program is created for COEN283 project.
+
+#include <allocation.h>
+#include <allocation.cpp>
+
+#ifndef FIRSTFIT_H_
+#define FIRSTFIT_H_
+
+class FirstFit: public Allocation
+{
+private:
+	//MEM_SIZE is 100, unsigned int
+	static unsigned int memArr[MEM_SIZE] = {0};
+
+	//keeps track of allocated location in memory
+	unsigned int memLoc = 0;
+
+public:
+	//constructor
+	FirstFit(unsigned int start, unsigned int size, unsigned int finish);
+
+	//destructor
+	~FirstFit();
+
+	//allocation
+	bool allocate();
+
+	//deallocation
+	bool deallocate();
+
+};
+
+#endif /* FIRSTFIT_H_ */
